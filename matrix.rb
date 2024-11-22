8079e0099cff37b102f9ab9a293044e2ab6842ce
Kasumi Hanazuki <kasumi@rollingapple.net>
2024-11-22 14:48:49 +0000

    typo

--- matrix.rb
+++ matrix.rb
@@ -31,7 +31,7 @@ matrix = matrix.map {|h|
   ruby = h.fetch(:ruby)
   cc = h[:compilers].fetch(:cc)
   h.merge(
-    artifact_tag: "ghcr.io/hanazuki/ruby-builder:ruby-#{ruby}_#{runner}_#{cc}}",
+    artifact_tag: "ghcr.io/hanazuki/ruby-builder:ruby-#{ruby}_#{runner}_#{cc}",
   )
 }
 
